8337293fa2cb1b5aeaa2e6f620d93f53d80f4a1f
ChanyanuchImk <chanyanuch.imk@dome.tu.ac.th>
2026-05-18 23:09:22 +0700

    add jwt authentication

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"restaurant-api/handlers"
+	"restaurant-api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+
+	r.POST("/login", handlers.Login)
+
+	auth := r.Group("/")
+
+	auth.Use(middleware.AuthMiddleware())
+
+}
