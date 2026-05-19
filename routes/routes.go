7c815a79ef7307cf0eb35217823d88bdd5d23fda
Pakin Maneerat <pakin.man@dome.tu.ac.th>
2026-05-19 22:31:00 +0700

    fix code

--- routes/routes.go
+++ routes/routes.go
@@ -9,9 +9,6 @@ import (
 
 func SetupRoutes(r *gin.Engine) {
 
-	// นำ ErrorHandler มาดักจับทุกเส้นทาง
-	r.Use(middleware.ErrorHandler())
-
 	r.POST("/login", handlers.Login)
 
 	auth := r.Group("/")
